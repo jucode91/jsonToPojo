2bde91593dea4cfae3223f87b5d6d844f01b2013
jucode91 <44717731+jucode91@users.noreply.github.com>
2018-11-03 09:26:16 +0530

    Add files via upload

---
+++ jsontopojo/src/main/java/jsontopojo/JsonToPojo.java
@@ -0,0 +1,53 @@
+package jsontopojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsonschema2pojo.DefaultGenerationConfig;
+import org.jsonschema2pojo.GenerationConfig;
+import org.jsonschema2pojo.Jackson2Annotator;
+import org.jsonschema2pojo.SchemaGenerator;
+import org.jsonschema2pojo.SchemaMapper;
+import org.jsonschema2pojo.SchemaStore;
+import org.jsonschema2pojo.SourceType;
+import org.jsonschema2pojo.rules.RuleFactory;
+
+import com.sun.codemodel.JCodeModel;
+
+public class JsonToPojo {
+
+	public static void main(String[] args) {
+		String packageName = "com.parse.result";		
+		File inputJson = new File("LOCATION OF YOUR JSON FILE");
+		File outputPojoDirectory = new File("convertedPojo");
+		outputPojoDirectory.mkdirs();
+		try {
+			new JsonToPojo().convert2JSON(inputJson.toURI().toURL(), outputPojoDirectory, packageName,
+					inputJson.getName().replace(".json", ""));
+		} catch (IOException e) {
+			System.out.println("Json parsing error : " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public void convert2JSON(URL inputJson, File outputPojoDirectory, String packageName, String className)
+			throws IOException {
+		JCodeModel codeModel = new JCodeModel();
+		URL source = inputJson;
+		GenerationConfig config = new DefaultGenerationConfig() {
+			@Override
+			public boolean isGenerateBuilders() { // set config option by overriding method
+				return true;
+			}
+
+			public SourceType getSourceType() {
+				return SourceType.JSON;
+			}
+		};
+		SchemaMapper mapper = new SchemaMapper(
+				new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
+		mapper.generate(codeModel, className, packageName, source);
+		codeModel.build(outputPojoDirectory);
+	}
+}
